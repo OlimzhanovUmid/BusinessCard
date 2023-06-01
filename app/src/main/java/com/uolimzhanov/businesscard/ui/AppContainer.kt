55818d440709e7c0a0fcd31c5d9bdd74d7547b74
Umid Olimzhanov <uolimzhanov@gmail.com>
2023-06-01 09:01:13 +0500

    Extracted string resources

--- app/src/main/java/com/uolimzhanov/businesscard/ui/AppContainer.kt
+++ app/src/main/java/com/uolimzhanov/businesscard/ui/AppContainer.kt
@@ -1,6 +1,5 @@
 package com.uolimzhanov.businesscard.ui
 
-import android.util.Log
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
@@ -25,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -61,7 +61,11 @@ fun AppContainer(
             MediumTopAppBar(
                 title = {
                     Text(
-                        text = selectedDestination
+                        text = when(selectedDestination){
+                            "Home" -> stringResource(id = R.string.home)
+                            "Info" -> stringResource(id = R.string.info)
+                            else -> stringResource(id = R.string.contacts)
+                        }
                     )
                 },
                 modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
