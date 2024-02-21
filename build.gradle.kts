17355f7f5e90d121e529e206f40722dfd82aa49e
Umid Olimzhanov <uolimzhanov@gmail.com>
2024-02-21 22:19:43 +0500

    gradle: update dependencies and migrate to version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
+plugins {
+    alias(libs.plugins.android.app) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.dagger) apply false
+}
+
 task("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,8 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
-    }
-
-    val versionAgp: String by settings
-    val versionKotlin: String by settings
-    val versionDagger: String by settings
-    val versionKsp: String by settings
-
-    plugins {
-        id("com.android.application") version versionAgp apply false
-        id("org.jetbrains.kotlin.android") version versionKotlin apply false
-        id("com.google.dagger.hilt.android") version versionDagger apply false
-        id("com.google.devtools.ksp") version versionKsp apply false
-        id("org.jetbrains.kotlin.plugin.parcelize") version versionKotlin apply false
     }
 }
 dependencyResolutionManagement {
@@ -26,5 +13,4 @@ dependencyResolutionManagement {
         maven(url = "https://www.jitpack.io")
     }
 }
-rootProject.name = "BusinessCard"
 include(":app")
