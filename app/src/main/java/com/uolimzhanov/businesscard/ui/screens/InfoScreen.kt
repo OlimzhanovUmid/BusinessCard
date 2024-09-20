89d38add93d6d46f0e2316a8ca69b5d14b99a49a
Umid Olimzhanov <uolimzhanov@gmail.com>
2024-09-20 15:00:34 +0500

    ui: optimize imports in info screen

--- app/src/main/java/com/uolimzhanov/businesscard/ui/screens/InfoScreen.kt
+++ app/src/main/java/com/uolimzhanov/businesscard/ui/screens/InfoScreen.kt
@@ -1,16 +1,11 @@
 package com.uolimzhanov.businesscard.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -18,11 +13,9 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.uolimzhanov.businesscard.R
-import com.uolimzhanov.businesscard.model.BadgeEvent
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
